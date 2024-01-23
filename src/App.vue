dcbe7b8d9230192262b795cfc684623e70b089ef
wangyi <haveaballmyball@hotmail.com>
2024-01-23 20:45:36 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+console.log(23424242)
+debugger
+const a = 1
+</script>
+
+<template>
+  <div>
+    <el-button type="primary">这是按钮</el-button>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+/** TODO:
+ * 1. 添加 vitest finish
+ * 2. 引入 element-plus finish
+ * 3. 配置 husky / format / editor 等
+ * 4. 删除原文件 finish
+ * 5. init layout
+ */
